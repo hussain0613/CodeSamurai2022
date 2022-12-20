92d71def5fc23dc1ad7e0ab888748761d1cc9497
hussain0613 <hussain0613@gmail.com>
2022-12-21 04:46:16 +0600

    added area based filtering option added api for fetching all locations modified get_projects to accept location and agency based filter also added offset and limit to them removed the type=module from script calls replaced defer with async 'agian'

--- backend/endpoints/projects.py
+++ backend/endpoints/projects.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.query import Query
 
 from app import sessionMaker
 from models.db import Project
@@ -9,12 +10,32 @@ from models.schema import ProjectSchema
 router = APIRouter(prefix = "/project", tags=["project"])
 
 @router.get("/get/")
-def get_projects():
+def get_projects(location: str = None, agency_code: str = None, offset: int = 0, limit: int = None):
     session: Session = sessionMaker()
 
-    projects_db: list[Project] = session.query(Project).all()
-    projects = list(map(ProjectSchema.from_orm, projects_db))
-
+    query: Query = session.query(Project)
+    if (location):
+        query = query.filter(Project.location == location)
+    if (agency_code):
+        query = query.filter(Project.exec_ == agency_code)
+    
+    query = query.offset(offset).limit(limit)
+    
+    projects_db: list[Project] = query.all()
+    
     session.close()
     
-    return projects
+    return projects_db
+
+@router.get("/get_locations/")
+def get_locations(offset: int = 0, limit: int = None):
+    session: Session = sessionMaker()
+
+    query: Query = session.query(Project.location).distinct(Project.location)
+    query = query.offset(offset).limit(limit)
+    
+    projects_locations_db: list[Project.location] = query.all()
+    projects_locations: list[str] = list(map(lambda proj: proj[0], projects_locations_db))
+    session.close()
+    
+    return projects_locations
--- www/scripts/index.js
+++ www/scripts/index.js
@@ -5,12 +5,13 @@ function get_set_project_markers(map){
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var projects = JSON.parse(this.responseText);
-      for(var i = 1; i < projects.length; ++i){
+      for(var i = 0; i < projects.length; ++i){
         set_marker(projects[i], map);
       }
     }
   };
-  xhttp.open("GET", SITE_SETTINGS["BASE_URL"] + "/project/get/", true);
+
+  xhttp.open("GET", SITE_SETTINGS["BASE_URL"] + "/project/get/" + location.search, true);
   xhttp.send();
 }
 
@@ -25,16 +26,35 @@ function set_marker(project, map){
 }
 
 function initMap() {
-    var default_center = {lat: 23.76, lng: 90.41 };
-    var map_zoom = 8;
+  var default_center = {lat: 23.76, lng: 90.41 };
+  var map_zoom = 8;
 
-    // The map, centered at default_center
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: map_zoom,
-      center: default_center,
-    });
-    get_set_project_markers(map);
-  }
+  // The map, centered at default_center
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: map_zoom,
+    center: default_center,
+  });
+  get_set_project_markers(map);
+}
 
-  window.initMap = initMap;
-  
+window.initMap = initMap;
+
+function fetch_location_list(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var project_locations = JSON.parse(this.responseText);
+      var filter_form = document.getElementById("filter_form");
+      var location_select = document.getElementById("location_select");
+      
+      for(var i = 1; i < project_locations.length; ++i){
+        var option = document.createElement("option");
+        option.innerText = project_locations[i];
+        option.value = project_locations[i];
+        location_select.appendChild(option);
+      }
+    }
+  };
+  xhttp.open("GET", SITE_SETTINGS["BASE_URL"] + "/project/get_locations/", true);
+  xhttp.send();
+}
